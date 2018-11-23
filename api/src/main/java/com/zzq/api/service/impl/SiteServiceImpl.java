7915786b5d6c5e41de1bb752d416f21bb22849b9
zzq <191550636@qq.com>
2018-11-23 11:36:09 +0800

    回溯版本查找问题

--- api/src/main/java/com/zzq/api/service/impl/SiteServiceImpl.java
+++ api/src/main/java/com/zzq/api/service/impl/SiteServiceImpl.java
@@ -11,6 +11,7 @@ import com.zzq.api.service.SiteService;
 import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.entity.Example;
 import tk.mybatis.mapper.util.Sqls;
+import tk.mybatis.mapper.util.StringUtil;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -52,21 +53,12 @@ public class SiteServiceImpl implements SiteService {
     @Override
     public Page<SiteVo> findPage(String uid, Integer num, Integer size) {
 
-        Example example = new Example.Builder(EmSite.class).where(Sqls.custom()
-                .andEqualTo("uid", uid)).build();
-
-        RowBounds rowBounds = new RowBounds(num, size);
-        List<EmSite> emSites = emSiteMapper.selectByExampleAndRowBounds(example, rowBounds);
-        System.out.println("==================*****************==============================");
-        for (EmSite emSite : emSites) {
-            System.out.println(emSite);
-        }
-        System.out.println(rowBounds.getLimit());
-        System.out.println(rowBounds.getOffset());
-        System.out.println("==================*****************==============================");
+        Example example = new Example.Builder(EmSite.class).build();
 
         PageHelper.startPage(num, size);
         List<EmSite> sites = emSiteMapper.selectByExample(example);
+        com.github.pagehelper.Page sites1 = (com.github.pagehelper.Page) (sites);
+        System.out.println(sites1);
         List<SiteVo> svoList = new ArrayList<>();
         sites.forEach(site -> svoList.add(new SiteVo(site)));
         System.out.println("************************************");
@@ -74,8 +66,13 @@ public class SiteServiceImpl implements SiteService {
             System.out.println(site);
         }
         System.out.println("************************************");
-        PageInfo<SiteVo> pageInfo = new PageInfo<>(svoList, 5);
-        System.out.println(pageInfo);
+        sites1.clear();
+        sites1.addAll(svoList);
+        System.out.println(sites1);
+//        PageInfo<SiteVo> pageInfo = new PageInfo<>(svoList, 5);
+        PageInfo<EmSite> pageInfo1 = new PageInfo<>(sites, 5);
+//        System.out.println(pageInfo);
+        System.out.println(pageInfo1);
 
         return new Page<>();
     }
